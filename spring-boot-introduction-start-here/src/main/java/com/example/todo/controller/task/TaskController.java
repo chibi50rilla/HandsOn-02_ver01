848f94393d822dff8bf0e27fd1ce1aed81bea56c
Nozomi Masuda <chibi.50rilla@gmail.com>
2026-01-30 15:07:31 +0900

    リスト一覧はIDではなくNo表示にする

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,13 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,11 +27,18 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public String showDetail(@PathVariable("id") long taskId, Model model) {
+    public String showDetail(
+            @PathVariable("id") long taskId,
+            @RequestParam(value = "no", required = false) Integer no,
+            Model model
+    ) {
         var taskDTO = taskService.findById(taskId)
                 .map(TaskDTO::toDTO)
                 .orElseThrow(TaskNotFoundException::new);
+
         model.addAttribute("task", taskDTO);
+        model.addAttribute("no", no); // ← 連番を渡す
+
         return "tasks/detail";
     }
 
