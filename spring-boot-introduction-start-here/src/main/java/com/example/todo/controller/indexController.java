e7c4f7ff5614ee41aaa620995c01874a89bc96f8
Nozomi Masuda <chibi.50rilla@gmail.com>
2026-02-04 19:17:53 +0900

    トップページの削除

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/indexController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/indexController.java
@@ -3,11 +3,13 @@ package com.example.todo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/")
+
+@Controller
 public class indexController {
 
-    @GetMapping
-    public String index(){
-        return "index";
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/tasks";
     }
 }
+
